05b2fab66e65085332763b92cd0295d544d31a70
Ali <heydari1375.ali@gmail.com>
2025-03-30 13:45:04 +0330

    add request to find movie id

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, HTTPException
+from dotenv import load_dotenv
+import requests
+import os
 
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
 
 app = FastAPI()
+
+@app.get('/similar-movies/')
+def get_similar_movies(movie_name:str = Query(..., description='movie name')):
+    if not API_KEY:
+        raise HTTPException(status_code=500, detail="API KEY MISSING")
+    
+    url = f'https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={movie_name}&language=fa'
+    response = requests.get(url)
+    data = response.json()
+
+    movie_id = data['results'][0]['id']
+
+    return movie_id
